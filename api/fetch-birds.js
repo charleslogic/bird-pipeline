cb70a023624408d16b2a726867476cc656dca4e4
Charles Richards <charles76012@gmail.com>
2026-04-21 12:48:21 -0500

    New debug code...

--- api/fetch-birds.js
+++ api/fetch-birds.js
@@ -1,3 +1,8 @@
-export default function handler(req, res) { 
-  res.status(200).json({ ok: true });
+export default function handler(req, res) {
+  try {
+    res.setHeader('Content-Type', 'application/json');
+    return res.status(200).send(JSON.stringify({ ok: true }));
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 }
