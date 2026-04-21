798e76c84f7ddd6f2c32cd584e2ca576b47e52ba
Charles Richards <charles76012@gmail.com>
2026-04-21 12:00:59 -0500

    maybe...

--- api/fetch-birds.js
+++ api/fetch-birds.js
@@ -1,22 +1,3 @@
-export default async function handler(req, res) {
-  const LAT = 32.7357;
-  const LNG = -97.1081;
-  const DIST = 25;
-
-  const EBIRD_API_KEY = process.env.EBIRD_API_KEY;
-
-  const url =
-    `https://api.ebird.org/v2/data/obs/geo/recent?lat=${LAT}&lng=${LNG}&dist=${DIST}`;
-
-  const r = await fetch(url, {
-    headers: { "X-eBirdApiToken": EBIRD_API_KEY } 
-  });
-
-  const data = await r.json();
-
-  res.status(200).json({
-    ok: true,
-    count: data.length,
-    sample: data[0] || null
-  });
+export default function handler(req, res) {
+  res.status(200).json({ ok: true });
 }
